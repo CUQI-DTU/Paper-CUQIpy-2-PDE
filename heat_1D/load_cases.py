cdd955ae4297a9fb1e8715dc6c4706aad37a95f1
amal-ghamdi <amal.m.alghamdi@gmail.com>
2022-12-16 10:04:21 +0100

    Add ability to read and plot prior samples

--- heat_1D/load_cases.py
+++ heat_1D/load_cases.py
@@ -3,7 +3,7 @@
 import pickle
 import matplotlib.pyplot as plt
 
-def load_case(case_dir, load_sol_data=False):
+def load_case(case_dir, load_sol_data=False, load_prior_samples=False):
     samples = pickle.load(open(case_dir + '/samples.pkl', 'rb'))
     parameters = pickle.load(open(case_dir + '/parameters.pkl', 'rb'))
     if load_sol_data:
@@ -20,30 +20,42 @@ def load_case(case_dir, load_sol_data=False):
         x_exact = None
         y_exact = None
         data = None
-    return samples, parameters, x_exact, y_exact, data
+    if load_prior_samples:
+        prior_samples = pickle.load(open(case_dir + '/prior_samples.pkl', 'rb'))
+    else:
+        prior_samples = None
 
+    return prior_samples, samples, parameters, x_exact, y_exact, data
 
-samples, parameters, x_exact, y_exact, data = load_case('./data2_cont3/paper_case17')
+if __name__ == '__main__':
+    prior_samples, samples, parameters, x_exact, y_exact, data = load_case('./data2_cont3/paper_case17')
+    
+    
+    case_list = []
+    case_list.append('./data2/paper_case2')
+    case_list.append('./data2_cont2/paper_case11')
+    case_list.append('./data2_cont2/paper_case12')
+    case_list.append('./data2_cont2/paper_case13')
+    case_list.append('./data2_cont2/paper_case14')
+    case_list.append('./data2_cont2/paper_case16')
+    case_list.append('./data2_cont3/paper_case17')
+    
+    load_sol_data_list = [False, False, False, False, False, False, True]
+    load_prior_samples_list = [False, False, False, False, False, False, True]
+    
+    for i, case in enumerate(case_list):
+        prior_samples, samples, parameters, x_exact, y_exact, data = load_case(case, load_sol_data_list[i], load_prior_samples_list[i])
+        print(parameters)
+        if load_sol_data_list[i]:
+            plt.figure()
+            x_exact.plot()
+            y_exact.plot()
+            data.plot()  
 
-#%%
-case_list = []
-case_list.append('./data2/paper_case2')
-case_list.append('./data2_cont2/paper_case11')
-case_list.append('./data2_cont2/paper_case12')
-case_list.append('./data2_cont2/paper_case13')
-case_list.append('./data2_cont2/paper_case14')
-case_list.append('./data2_cont2/paper_case16')
-case_list.append('./data2_cont3/paper_case17')
-
-load_sol_data_list = [False, False, False, False, False, False, True]
-
-for i, case in enumerate(case_list):
-    samples, parameters, x_exact, y_exact, data = load_case(case, load_sol_data_list[i])
-    print(parameters)
-    if load_sol_data_list[i]:
-        plt.figure()
-        x_exact.plot()
-        y_exact.plot()
-        data.plot()        
-
-    print('_'*80)
+        if load_prior_samples_list[i]:
+            plt.figure()
+            for s in prior_samples:
+                prior_samples.geometry.plot(s, is_par=True)    
+    
+        print('_'*80)
+    
