d5268862af6e1f793c0b123cada68a2a4a610517
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-02-02 09:29:04 +0100

    print to give overview of cases parameters

--- heat_1D/load_cases.py
+++ heat_1D/load_cases.py
@@ -2,6 +2,7 @@
 #%%
 import pickle
 import matplotlib.pyplot as plt
+from figures_util import print_parameters_that_are_not_the_same
 
 def load_case(case_dir, load_sol_data=False, load_prior_samples=False):
     samples = pickle.load(open(case_dir + '/samples.pkl', 'rb'))
@@ -31,31 +32,53 @@ if __name__ == '__main__':
     prior_samples, samples, parameters, x_exact, y_exact, data = load_case('./data2_cont3/paper_case17')
     
     
+   # case_list = []
+   # case_list.append('./data2/paper_case2')
+   # case_list.append('./data2_cont2/paper_case11')
+   # case_list.append('./data2_cont2/paper_case12')
+   # case_list.append('./data2_cont2/paper_case13')
+   # case_list.append('./data2_cont2/paper_case14')
+   # case_list.append('./data2_cont2/paper_case16')
+   # case_list.append('./data2_cont3/paper_case17')
+
     case_list = []
-    case_list.append('./data2/paper_case2')
-    case_list.append('./data2_cont2/paper_case11')
-    case_list.append('./data2_cont2/paper_case12')
-    case_list.append('./data2_cont2/paper_case13')
-    case_list.append('./data2_cont2/paper_case14')
-    case_list.append('./data2_cont2/paper_case16')
-    case_list.append('./data2_cont3/paper_case17')
+    case_list.append('./data2_cont6/paper_case2_b')
+    case_list.append('./data2_cont6/paper_case2_b2')
+    case_list.append('./data2_cont6/paper_case2_b3')
+    case_list.append('./data2_cont6/paper_case2_b4')
+    case_list.append('./data2_cont6/paper_case2_b3_1')
+    case_list.append('./data2_cont6/paper_case2_b3_2')
+    case_list.append('./data2_cont6/paper_case2_b3_3')
+    case_list.append('./data2_cont6/paper_case2_b3_4')
+    case_list.append('./data2_cont6/paper_case2_b3_5')
+    case_list.append('./data2_cont6/paper_case2_b5')
+    case_list.append('./data2_cont6/paper_case2_b6')
+    case_list.append('./data2_cont6/paper_case2_b6_2')
+    case_list.append('./data2_cont3/paper_case3')
+    case_list.append('./data2_cont5/paper_case3_c')
     
-    load_sol_data_list = [False, False, False, False, False, False, True]
-    load_prior_samples_list = [False, False, False, False, False, False, True]
-    
+    load_sol_data_list = [False, False, False, False, False, False, True, True, True, True, True, True, False, False]
+    load_prior_samples_list = [False, False, False, False, False, False, True, True, True, True, True, True, False, False]
+
+    parameters_list = []
+    generate_plot = False
     for i, case in enumerate(case_list):
         prior_samples, samples, parameters, x_exact, y_exact, data = load_case(case, load_sol_data_list[i], load_prior_samples_list[i])
-        print(parameters)
-        if load_sol_data_list[i]:
+
+        parameters_list.append(parameters)
+        if load_sol_data_list[i] and generate_plot:
             plt.figure()
             x_exact.plot()
             y_exact.plot()
             data.plot()  
 
-        if load_prior_samples_list[i]:
+        if load_prior_samples_list[i] and generate_plot:
             plt.figure()
             for s in prior_samples:
                 prior_samples.geometry.plot(s, is_par=True)    
-    
-        print('_'*80)
+
+        #print(parameters)
+        #print('_'*80)
+
+    print_parameters_that_are_not_the_same(parameters_list, ['ESS', 'updated_scale', 'scale'], ['exact_func'])
     
