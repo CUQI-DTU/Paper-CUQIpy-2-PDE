bf851640c94876e63cb9920a45988fbbc03f28be
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-02-10 14:39:47 +0100

    Add matplotlib_setup method

--- heat_1D/figures_util.py
+++ heat_1D/figures_util.py
@@ -1,6 +1,7 @@
 
 # %%
 import numpy as np
+import matplotlib.pyplot as plt
 
 def collect_all_keys(case_list):
     keys = []
@@ -40,4 +41,12 @@ def print_parameters_that_are_not_the_same(case_list, ignore_keys=[], include_ke
             except KeyError:
                 print("No key",key,"in case",case['case_name'])
 
-    
+
+def matplotlib_setup(SMALL_SIZE, MEDIUM_SIZE, BIGGER_SIZE):
+    plt.rc('font', size=MEDIUM_SIZE)         # controls default text sizes
+    plt.rc('axes', titlesize=BIGGER_SIZE)    # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title 
